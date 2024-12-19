caec6abf6162936085165b0892c4e2f76601692c
Riya Prajapati <riyadijju@gmail.com>
2024-12-19 14:12:34 +0545

    Added Nav headers
    Search, Cart and User btn

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -7,6 +7,8 @@ const Navbar = () => {
     <nav className ='max-w-screen-2xl mx-auto px-4 flex justify-between items-center'>
         <ul className='nav__links'>
          
+         {/* NAV HEADS */}
+
         <li> <Link to="/">Home</Link> </li>
         <li> <Link to="/shop">Shop</Link> </li>
         <li> <Link to="/blogs">Blogs</Link> </li>
@@ -19,6 +21,34 @@ const Navbar = () => {
             <img src="/prajapati logo.png" alt="Prajapati Ceramic Logo" className="w-24 h-auto" />
           </Link>
         </div>
+
+        {/* NAV ICONS */}
+        <div className='nav__icons relative'>
+
+          {/* -------Search Button---------- */}
+          <span>
+            <Link to ="/search" >
+              <i className="ri-search-line"></i>
+            </Link>
+          </span> 
+
+          {/* -------Cart Button---------- */}
+          <span>
+            <button className='hover:text-primary'>
+              <i className="ri-shopping-cart-line"></i>
+              <sup className='text-sm inline-block px-1.5 text-white rounded-full bg-primary text-center'>0</sup>
+            </button>
+          </span>
+          {/* -------User Button--------- */}
+          <span>
+            <Link to="login">
+            <i class="ri-user-6-line"></i>
+            </Link>
+          </span>
+
+
+          </div>
+
     </nav>
     </header>
   )
