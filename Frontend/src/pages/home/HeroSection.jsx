544338156639ee580be53563047c73672469abd9
Riya Prajapati <riyadijju@gmail.com>
2024-12-25 16:33:01 +0545

    updated images

--- Frontend/src/pages/home/HeroSection.jsx
+++ Frontend/src/pages/home/HeroSection.jsx
@@ -1,31 +1,44 @@
 import React from 'react'
-import card1 from "../../assests/card-1.png"
-import card2 from "../../assests/card-2.png"
-import card3 from "../../assests/card-3.png"
+
+import card1 from "../../assets/card-1.png"
+import card2 from "../../assets/card-2.png"
+import card3 from "../../assets/card-3.png"
 
 const cards =[
     { 
         id:1,
         image: card1,
+        trend: '2025 Trend',
         title: 'Gautam Buddha Statue'
     },
     { 
         id:2,
         image: card2,
-        title: 'Gautam Buddha Statue'
+        trend: '2025 Trend',
+        title: 'Home Decor'
     },
     { 
         id:3,
         image: card3,
+        trend: '2025 Trend',
         title: 'Gautam Buddha Statue'
     }
 
 ]
 const HeroSection = () => {
   return (
-    <div>
-      Hero Section
-    </div>
+    <section className='section__container hero__container'>
+        {
+            cards.map((card)=>(
+                <div key={card.id} className='hero__card'>
+                    <img src={card.image} alt= ""/>
+
+                </div>
+            ))
+
+        }
+
+    </section>
   )
 }
 
