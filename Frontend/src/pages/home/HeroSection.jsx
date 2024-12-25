2fb4854b769dcbb854993f994bd40c030b9c10a8
Riya Prajapati <riyadijju@gmail.com>
2024-12-25 15:56:11 +0545

    Update HeroSection.jsx

--- Frontend/src/pages/home/HeroSection.jsx
+++ Frontend/src/pages/home/HeroSection.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import card1 from "../../assests/card-1.png"
+import card2 from "../../assests/card-2.png"
+import card3 from "../../assests/card-3.png"
 
+const cards =[
+    { 
+        id:1,
+        image: card1,
+        title: 'Gautam Buddha Statue'
+    },
+    { 
+        id:2,
+        image: card2,
+        title: 'Gautam Buddha Statue'
+    },
+    { 
+        id:3,
+        image: card3,
+        title: 'Gautam Buddha Statue'
+    }
+
+]
 const HeroSection = () => {
   return (
     <div>
-      
+      Hero Section
     </div>
   )
 }
