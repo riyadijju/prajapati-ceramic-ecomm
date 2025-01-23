29e67e86d9e2d91cdd97fb464ed6a2fe37629f00
Riya Prajapati <riyadijju@gmail.com>
2025-01-23 15:33:32 +0545

    Updated PromoBanner.jsx

--- Frontend/src/pages/home/PromoBanner.jsx
+++ Frontend/src/pages/home/PromoBanner.jsx
@@ -4,8 +4,22 @@ import React from 'react'
 const PromoBanner = () => {
   return (
     <section className='section__container banner__container'>
-        <div className=''>
-            </div>    </section>
+        <div className='banner__card'>
+           <span> <i className="ri-truck-line"></i></span>
+           <h4>Free Deliver</h4>
+           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, eveniet facilis quo eum quam fugiat incidunt, dolor amet libero nihil officiis provident earum voluptates cumque? Cumque nostrum repellendus soluta commodi.</p>
+        </div>    
+        <div className='banner__card'>
+           <span> <i class="ri-money-dollar-circle-line"></i></span>
+           <h4>Free Deliver</h4>
+           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, eveniet facilis quo eum quam fugiat incidunt, dolor amet libero nihil officiis provident earum voluptates cumque? Cumque nostrum repellendus soluta commodi.</p>
+        </div>   
+        <div className='banner__card'>
+           <span> <i className="ri-truck-line"></i></span>
+           <h4>Free Deliver</h4>
+           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, eveniet facilis quo eum quam fugiat incidunt, dolor amet libero nihil officiis provident earum voluptates cumque? Cumque nostrum repellendus soluta commodi.</p>
+        </div>   
+    </section>
   )
 }
 
