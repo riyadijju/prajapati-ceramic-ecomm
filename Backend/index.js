2ee850be70ae1cce0f843417c217a85b3bca52d3
Riya Prajapati <riyadijju@gmail.com>
2025-03-22 12:19:05 +0545

    - [x] dotenv, cors, cookie parser and body parser packages

--- Backend/index.js
+++ Backend/index.js
@@ -1,15 +1,31 @@
-const express = require('express')
-const mongoose = require('mongoose');
-const app = express()
-const port = process.env.PORT || 5000;
-
 //admin
 // YY1QsdPcPSMa29Tb
 
-main().then(() => console.log("Mongodb is successful.")).catch(err => console.log(err));
+const express = require('express')
+const mongoose = require('mongoose');
+const cors = require('cors')
+const app = express()
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+// middleware setup
+app.use(express.json({limit: "25mb"}));
+app.use((express.urlencoded({limit: "25mb"})));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
+
+main().
+then(() => 
+    console.log("Mongodb is successful."))
+.catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect('mongodb+srv://admin:YY1QsdPcPSMa29Tb@prajapati-ceramic.jnkgn.mongodb.net/?retryWrites=true&w=majority&appName=prajapati-ceramic');
+    await mongoose.connect(process.env.DB_URL);
 
     app.get('/', (req, res) => {
         res.send('Prajapati Ceramic')
