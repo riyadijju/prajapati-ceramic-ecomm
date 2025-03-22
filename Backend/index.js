608da5b9997efd2046ace505975ed8220ab68f1f
Riya Prajapati <riyadijju@gmail.com>
2025-03-22 11:59:54 +0545

    Updated index.js

--- Backend/index.js
+++ Backend/index.js
@@ -6,15 +6,15 @@ const port = process.env.PORT || 5000;
 //admin
 // YY1QsdPcPSMa29Tb
 
-main().catch(err => console.log(err));
+main().then(() => console.log("Mongodb is successful.")).catch(err => console.log(err));
 
 async function main() {
     await mongoose.connect('mongodb+srv://admin:YY1QsdPcPSMa29Tb@prajapati-ceramic.jnkgn.mongodb.net/?retryWrites=true&w=majority&appName=prajapati-ceramic');
-}
 
-app.get('/', (req, res) => {
-  res.send('Prajapati Ceramic')
-})
+    app.get('/', (req, res) => {
+        res.send('Prajapati Ceramic')
+      })
+}
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
