e6e27b9f2874746b93bd4ffb2e400953e2982e28
Riya Prajapati <riyadijju@gmail.com>
2025-03-23 18:16:12 +0545

    updated index.js

---
+++ Backend/src/users/user.route.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+// Register endpoint
+router.post("/register", async (req, res) => {
+    try {
+      const { username, email, password } = req.body;
+      const user = new User({ email, username, password });
+      await user.save();
+      res.status(201).send({ message: "User registered successfully!" });
+    } catch (error) {
+      console.error("Error registering user", error);
+      res.status(500).send({ message: "Error registering user" });
+    }
+  });
+
+module.exports = router;
