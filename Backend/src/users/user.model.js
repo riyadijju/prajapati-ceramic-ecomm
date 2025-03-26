37aa0548859340affbd874c52a9c4b7c81d042bb
Riya Prajapati <riyadijju@gmail.com>
2025-03-26 17:00:43 +0545

    Updated user.model.js

--- Backend/src/users/user.model.js
+++ Backend/src/users/user.model.js
@@ -1,4 +1,5 @@
 const {Schema, model} = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userShecma = new Schema({
     username: {type: String, require: true, unique: true},
@@ -15,6 +16,20 @@ const userShecma = new Schema({
         default: Date.now
     }
 });
+
+// hashing passwords
+userShecma.pre('save', async function(next){
+    const user =  this;
+    if(!user.isModified('password')) return next();
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+    user.password = hashedPassword;
+    next();
+})
+
+// match passwords
+userShecma.methods.comparePassword = function (cadidatePassword) {
+    return bcrypt.compare(cadidatePassword, this.password)
+}
 
 const User = new model('User', userShecma);
 module.exports = User;
--- Backend/src/users/user.route.js
+++ Backend/src/users/user.route.js
@@ -11,9 +11,25 @@ router.post("/register", async (req, res) => {
     res.status(201).send({ message: "User registered successfully!" });
     
   } catch (error) {
-    // console.error("Error registering user", error);
-    // res.status(500).send({ message: "Error registering user" });
+    console.error("Error registering user", error);
+    res.status(500).send({ message: "Error registering user" });
   }
 });
+
+// / login user endpoint
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  console.log(email, password);
+  
+  const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).send({ message: "User not found" });
+    }
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+      return res.status(401).send({ message: "Password not match" });
+    }
+
+})
 
 module.exports = router;
