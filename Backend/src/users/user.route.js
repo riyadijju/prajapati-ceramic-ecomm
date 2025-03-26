e992d72a8809144520af87fb45340bb2448ebc60
Riya Prajapati <riyadijju@gmail.com>
2025-03-26 13:20:31 +0545

    Added user.routes.js

--- Backend/src/users/user.route.js
+++ Backend/src/users/user.route.js
@@ -1,17 +1,19 @@
 const express = require("express");
+const User = require("./user.model");
 const router = express.Router();
 
 // Register endpoint
 router.post("/register", async (req, res) => {
-    try {
-      const { username, email, password } = req.body;
-      const user = new User({ email, username, password });
-      await user.save();
-      res.status(201).send({ message: "User registered successfully!" });
-    } catch (error) {
-      console.error("Error registering user", error);
-      res.status(500).send({ message: "Error registering user" });
-    }
-  });
+  try {
+    const { username, email, password } = req.body;
+    const user = new User({ email, username, password });
+    await user.save();
+    res.status(201).send({ message: "User registered successfully!" });
+    
+  } catch (error) {
+    // console.error("Error registering user", error);
+    // res.status(500).send({ message: "Error registering user" });
+  }
+});
 
 module.exports = router;
