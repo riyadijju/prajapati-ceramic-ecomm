f24db4ac37bf987980ccc3c7515cffe28acf7fd5
Riya Prajapati <riyadijju@gmail.com>
2025-03-27 15:04:27 +0545

    Verify Admin middleware

--- Backend/src/middleware/verifyToken.js
+++ Backend/src/middleware/verifyToken.js
@@ -5,9 +5,9 @@ const JWT_SECRET = process.env.JWT_SECRET_KEY;
 
 const verifyToken = (req, res, next) => {
     try {
-        const token = req.cookies.token;
+        // const token = req.cookies.token;
         
-        // const token = req.headers["authorization"].split(" ")[1]
+        const token = req.headers["authorization"].split(" ")[1]
        
         if(!token) {
             return res.status(401).send({message: 'Invalid token'})
