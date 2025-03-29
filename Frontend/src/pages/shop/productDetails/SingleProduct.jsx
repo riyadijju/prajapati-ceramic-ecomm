eb13db69a41f5ff599864ce75306fc2306610bb9
Riya Prajapati <riyadijju@gmail.com>
2025-03-29 13:41:14 +0545

    Update SingleProduct.jsx

--- Frontend/src/pages/shop/productDetails/SingleProduct.jsx
+++ Frontend/src/pages/shop/productDetails/SingleProduct.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import { Link, useParams } from 'react-router-dom'
-import productImage from '../../../assets/image.png' 
 import RatingStars from '../../../components/RatingStars';
+import {useDispatch} from "react-redux"
+import { useFetchProductByIdQuery } from '../../../redux/features/products/productsApi';
+import { addToCart } from '../../../redux/features/cart/cartSlice';
+// import ReviewsCard from '../reviews/ReviewsCard';
 
 const SingleProduct = () => {
     const { id } = useParams();
-    console.log(id);
+
+    const dispatch =  useDispatch();
+    const {data, error, isLoading} = useFetchProductByIdQuery(id);
+    console.log(data);
+   
+    const singleProduct = data?.product || {};
+    const productReviews = data?.reviews || [];
+
+    const handleAddToCart = (product) => {
+        dispatch(addToCart(product))
+    }
+
+    if(isLoading) return <p>Loading...</p>
+    if(error) return <p>Error loading product details.</p>
+
 
     return (
         <>
@@ -16,7 +33,7 @@ const SingleProduct = () => {
                     <i className="ri-arrow-right-s-line"></i>
                     <span className='hover:text-primary'><Link to="/shop">shop</Link></span>
                     <i className="ri-arrow-right-s-line"></i>
-                    <span className='hover:text-primary'>name</span>
+                    <span className='hover:text-primary'>{singleProduct.name}</span>
                 </div>
             </section>
 
@@ -25,32 +42,39 @@ const SingleProduct = () => {
                     {/* Product Image */}
                     <div className='md:w-1/2 w-full'>
                         <img 
-                            src={productImage} 
+                            src={singleProduct?.image} 
                             alt='Chapacho Vase' 
                             className='rounded-md w-full h-auto'
                         />
                     </div>
 
                     <div className='md:w-1/2 w-full'>
-                        <h3 className='text-2xl font-semibold mb-4'>Chapacho Vase</h3>
-                        <p className='text-xl text-primary mb-4 space-x-1'>Rs. 2500 <s>Rs. 3500</s></p>
-                        <p className='text-gray-700 mb-4'>
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam possimus debitis tempora cupiditate delectus animi, eveniet recusandae necessitatibus nobis voluptates nihil. Vel ipsa a voluptas, tenetur deserunt iure corporis eligendi!
-                        </p>
+                        <h3 className='text-2xl font-semibold mb-4'>{singleProduct?.name}</h3>
+                        <p className='text-xl text-primary mb-4 space-x-1'>
+                            Rs. {singleProduct?.price} 
+                             {singleProduct?.oldPrice && <s className='ml-3'>Rs. {singleProduct?.oldPrice}</s>}
+                            </p>
+                            <p className='text-gray-400 mb-4'>{singleProduct?.description}</p>
 
                         {/* additional product info */}
                         <div className='flex flex-col space-y-2'>
-                            <p><strong>Category:</strong> Decor</p>
-                            <p><strong>Color:</strong> Beige</p>
+                            <p><strong>Category:</strong> {singleProduct?.category}</p>
+                            <p><strong>Color:</strong> {singleProduct?.color}</p>
                             <div className='flex gap-1 items-center'>
                                 <strong>Rating: </strong>
-                                <RatingStars rating={"5"}/>
+                                <RatingStars rating={singleProduct?.rating}/>
                             </div>
-                    </div>
+                           
+                        </div>
 
-                    <button className='mt-6 px-6 py-3 bg-black text-white rounded-md'>
-                        Add to Cart
-                    </button>
+                        <button 
+                        onClick={(e) => {
+                            e.stopPropagation();
+                            handleAddToCart(singleProduct)
+                        }}
+                        className='mt-6 px-6 py-3 bg-primary text-white rounded-md'>
+                            Add to Cart
+                        </button>
                 </div>
                 </div>
             </section>
