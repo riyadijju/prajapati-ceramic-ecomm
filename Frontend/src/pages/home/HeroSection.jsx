35fee71312832488fedbd5bdac378bd1dde22ed6
Riya Prajapati <riyadijju@gmail.com>
2025-04-22 10:29:06 +0545

    Update HeroSection.jsx

--- Frontend/src/pages/home/HeroSection.jsx
+++ Frontend/src/pages/home/HeroSection.jsx
@@ -1,33 +1,6 @@
 import React from 'react'
 import budh from "../../assets/budh.png"
 
-
-
-import card1 from "../../assets/card-1.png"
-import card2 from "../../assets/card-2.png"
-import card3 from "../../assets/card-3.png"
-
-// const cards =[
-//     { 
-//         id:1,
-//         image: card1,
-//         trend: '2025 Trend',
-//         title: 'Gautam Buddha Statue'
-//     },
-//     { 
-//         id:2,
-//         image: card2,
-//         trend: '2025 Trend',
-//         title: 'Home Decor'
-//     },
-//     { 
-//         id:3,
-//         image: card3,
-//         trend: '2025 Trend',
-//         title: 'Gautam Buddha Statue'
-//     }
-
-// ]
 const HeroSection = () => {
   return (
     <section className='section__container relative w-[80%] !px-[4.5rem]'>
