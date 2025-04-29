b718ac673370db3c138f44a710672cfc14de3127
Riya Prajapati <riyadijju@gmail.com>
2025-04-29 09:34:42 +0545

    Updated products.model.js

--- Backend/src/products/products.model.js
+++ Backend/src/products/products.model.js
@@ -1,31 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const VariantSchema = new mongoose.Schema({
-//   name: String,
-//   color: String,
-//   image: String,
-//   price: Number,
-//   stock: Number
-// });
-
-// const ProductSchema = new mongoose.Schema(
-//   {
-//     name: { type: String, required: true },
-//     category: { type: String, required: true },
-//     description: { type: String, required: true },
-//     price: { type: Number, required: true },
-//     image: { type: String, required: true }, // main image
-//     rating: { type: Number, default: 0 },
-//     variants: [VariantSchema],
-//     author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
-//   },
-//   { timestamps: true }
-// );
-
-// const Products = mongoose.model("Product", ProductSchema);
-
-// module.exports = Products;
-
 const mongoose = require("mongoose");
 
 const VariantSchema = new mongoose.Schema({
